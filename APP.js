06e04195ff131efdbe970e12f0eb5b726b2cb631
ArpanAdhikari2 <arpanadhikarisu24@gmail.com>
2026-04-17 20:03:14 +0530

    jsx components

--- APP.js
+++ APP.js
@@ -1,19 +1,30 @@
-const heading = React.createElement(
-    "h1",
-    {id:"heading"},
-    "hello world from react!"
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// the classic way to create a element in reat
+// const heading = React.createElement(
+//     "h1",
+//     {id:"heading"},
+//     "hello world from react!"
+// );
+// console.log(heading);
+
+//jsx way
+//jsx()
+const jsxHead=<h1 id="head">Arpan follow the way</h1>
+const Inside=() =>(
+    <h2>nested shit</h2>
 );
-const parent= React.createElement(
-    "div",
-    {id: "parent"},
-    React.createElement(
-        "div",
-        {id: "child"},
-        [React.createElement("h1",{},"IM THE REAL ONE "),
-        React.createElement("h1",{},"IM THE REAL ONE ")]
-    )
-)
-console.log(parent);
+
+// component composition
+const Real=() => (
+    <div id="jainwin">
+        <h1>my is arpan</h1>
+        <Inside />
+    </div>
+);
+
+console.log(jsxHead);
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<Real />);
---
+++ README.md
@@ -0,0 +1,5 @@
+# this is my first lecture in react js
+
+# PARCEL AND ITS FUNCTIONS 
+- ITS A BUNDLER
+- 
